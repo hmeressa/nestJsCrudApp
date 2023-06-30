7ddcaa5b4fd1d61f461cd484ad2029a3da2861b6
hiluf.meressa@ienetworksolutions.com <hmkahsay@gmail.com>
2023-06-30 10:45:46 +0300

    i some comments to the existing nest js applications

--- src/Config/connectionEnv.ts
+++ src/Config/connectionEnv.ts
@@ -3,7 +3,6 @@ import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 
 export const DatabaseConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    /* host: 'host.docker.internal',*/
     host: 'localhost',
     port: 5432,
     username: 'postgres',
